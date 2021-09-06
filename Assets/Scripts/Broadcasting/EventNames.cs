6ef8224757725fbe39bc0da913b4669c630fdaa7
Rethaniel Ramos <rethanielramos@gmail.com>
2021-09-07 00:55:44 +0800

    import event broadcasting scripts

---
+++ Assets/Scripts/Broadcasting/EventNames.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+/*
+ * Holder for event names
+ * Created By: NeilDG
+ */
+public class EventNames
+{
+
+    public class ItemEvents
+    {
+        public const string ON_ITEM_PICKUP = "ON_ITEM_PICKUP";
+    }
+}
